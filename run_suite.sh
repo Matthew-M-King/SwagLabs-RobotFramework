b7dc672401e6656818da3b46cb3ccdfe73d378a5
Matthew King <matthewking182@gmail.com>
2024-08-04 10:48:43 +0100

    update

--- run_suite.sh
+++ run_suite.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -x
-echo "ENV_SETUP"
-echo $ENV_SETUP
-echo "PLATFORM"
-echo $PLATFORM
-source $ENV_SETUP
 export LD_LIBRARY_PATH=$$LDLIBRARY_PATH:/usr/lib/x86_64-linux-gnu/odbc/
 ROBOT_OPTIONS="--output output/output.xml --log output/log.html --report output/report.html"
 if [ "$DRY_RUN" = "true" ]; then
