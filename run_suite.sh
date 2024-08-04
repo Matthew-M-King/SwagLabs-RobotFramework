170772802bb9f8358dbaed7e0e5aa3a499c8435d
Matthew King <matthewking182@gmail.com>
2024-08-04 11:23:32 +0100

    update

--- run_suite.sh
+++ run_suite.sh
@@ -1,10 +1,3 @@
-
+#!/bin/bash -x
 export LD_LIBRARY_PATH=$$LDLIBRARY_PATH:/usr/lib/x86_64-linux-gnu/odbc/
-# ROBOT_OPTIONS="--output output/output.xml --log output/log.html --report output/report.html"
-# if [ "$DRY_RUN" = "true" ]; then
-#     ROBOT_OPTIONS+=" --dryrun"
-# fi
-# export ROBOT_OPTIONS
-# robot $ROBOT_OPTIONS -L trace -v target_sut:$PLATFORM $@ || true
-
 robot -d reports .
